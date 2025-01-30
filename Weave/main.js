2aad0d168b1c9e5dc730f744667ec5faee17d096
loganwipper <loganwipper1@gmail.com>
2025-01-30 13:08:48 -0600

    Overlayed UI over Canvas

--- Weave/main.js
+++ Weave/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b59ac88e54b8d39228413915efee7441b4eabe1a947701dce996ddf40742e833
-size 798
+oid sha256:3e75c120293a6c7a3ae903ae5884622f08ed6c20640d4c1486dd0b60f4782b9d
+size 782
