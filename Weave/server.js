3cbdbe6bdc35e08a725aaf2edd012c0154f50dc7
Kaiser <nicholasjckaiser@gmail.com>
2025-02-12 10:10:50 -0600

    Added network functionalities to drawings

--- Weave/server.js
+++ Weave/server.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1db096fb9e8b3db234a9a3cb22f7499239ac4fd51de88dabd2847b98da9230ab
-size 558
+oid sha256:0151b7870c9071d3031e494d41f79ae066c81d73ed1c702df3b0ac761e3d0eb0
+size 725
