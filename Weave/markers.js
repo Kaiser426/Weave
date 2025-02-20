3038868fd4bb75c46ab2ad95863d5a4a181ffd16
Kaiser <nicholasjckaiser@gmail.com>
2025-02-20 15:18:22 -0600

    Left clicking markers and scrolling doesnt zoom in and out now

--- Weave/markers.js
+++ Weave/markers.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c4a8f1425ecba5401a4b0766bc22bb0dd23dc5a168b6b543f6a684629f2e66e
-size 3256
+oid sha256:4e578e288307617f956a73c737171ff15b5315aaf7dc2b8b884224c7663815c3
+size 3311
--- Weave/renderer.js
+++ Weave/renderer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:258655615ee9888a87f19410a3f602cacbd8da841825cea580932e3d7597d6fe
-size 6781
+oid sha256:523f1946703938d4f223f196aecef6ce950796040defecabe16212b98a79f524
+size 6841
