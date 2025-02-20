fe788c35b2c2cb9094d4c84b843c24da7bfdb4cb
loganwipper <loganwipper1@gmail.com>
2025-02-20 13:12:12 -0600

    Addition of Read and Write function

--- Weave/notesTempFile.js
+++ Weave/notesTempFile.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df58f4f660305228909a258e770e714d20d83c782e21eeba4263acc33ae09b9b
-size 1412
+oid sha256:43bec8f37955db43a04e4e2d597adf34fb9f91f143df28c0e0abd6067db55e10
+size 2493
--- Weave/server.js
+++ Weave/server.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1db096fb9e8b3db234a9a3cb22f7499239ac4fd51de88dabd2847b98da9230ab
-size 558
+oid sha256:ccc26636833eabf2447f4604444946876b3db4951fd4458844110ee0d2fa5a1f
+size 1409
