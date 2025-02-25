27644d276dfc1c31eda4aefa8e605063f474e06a
thojf <165195340+thojf@users.noreply.github.com>
2025-02-24 21:37:06 -0600

    Update server.js

--- Weave/server.js
+++ Weave/server.js
@@ -1,3 +1,50 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4fe10cf7308473d7db31cba4a1ed155f90af27c86a074664b80e41759a0083a7
-size 1731
+// server.js
+
+const http = require('http').createServer();
+const io = require('socket.io')(http, {
+    cors: {origin: "*"}
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('dragMoveToken', (data) => {
+        socket.broadcast.emit('dragMoveToken', data); // Send to all clients except the sender
+    });
+    socket.on('startUpdatingDrawing', (data) => {
+        socket.broadcast.emit('startUpdatingDrawing', data);
+    })
+    socket.on('updateDrawing', (data) => {
+        socket.broadcast.emit('updateDrawing', data);
+    });
+    socket.on('clearAllDrawings', () => {
+        socket.broadcast.emit('clearAllDrawings');
+    })
+    socket.on('updateMap', (imageURL) => {
+        socket.broadcast.emit('updateMap', imageURL);
+    });
+    
+    const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+    socket.on('chatMessage', (msg) => {
+        console.log('Server received:', msg);
+        io.emit('chatMessage', msg);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server on port 3000');
+});
+
+});
+
+
+http.listen(8080, () => console.log('listening on http://localhost:8080'));
