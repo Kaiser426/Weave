8ebdafecfbfd6eefedae4505097939fe7ffb793c
Kaiser <nicholasjckaiser@gmail.com>
2025-02-24 23:03:55 -0600

    Fixed bugs: chat was not sending messages chat was not drag and moving header bar on notes wasn't using the css class #notesHeader removed the script from index.html

--- Weave/notesTempFile.js
+++ Weave/notesTempFile.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43bec8f37955db43a04e4e2d597adf34fb9f91f143df28c0e0abd6067db55e10
-size 2493
+oid sha256:5314e6fc7bb60a10381886d595273ba2d8e464c3469e773ca5364dbd12a13364
+size 2489
--- Weave/renderer.js
+++ Weave/renderer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:523f1946703938d4f223f196aecef6ce950796040defecabe16212b98a79f524
-size 6841
+oid sha256:91ec4fc34148560c01e21b9844a2bec8d5ca9a3598c75421d4d5e76f7e249638
+size 7205
