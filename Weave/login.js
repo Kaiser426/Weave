93363f2d005ee4124ea2cccf98ff9e0091bb85cd
Kaiser <nicholasjckaiser@gmail.com>
2025-03-20 14:46:09 -0500

    Integrated Will's changes.

---
+++ Weave/login.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:50095f71bc89a3d3a5bea14556d7bff7162aa5f8571f2783dc969532705f68e6
+size 419
--- Weave/main.js
+++ Weave/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b7613f3514593e9e2a2f71c42f5bab8948b21bc89f317ab1011bee0e127a971
-size 783
+oid sha256:fae3d12f870f5e67446288d97f05b7a67ff38f39d1034dca2e0a81bcce554153
+size 1021
